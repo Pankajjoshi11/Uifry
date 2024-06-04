1514d8485ea0561c27253ce37a8c657c30422f3b
Pankaj Joshi <pankaj70451@gmail.com>
2024-06-05 01:59:30 +0530

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
         <div className="flex p-2 m-4">
           <button 
             className='bg-black text-white rounded-sm px-4 py-2 hover:bg-gray-700'>
-            Get Started ->
+            Get Started 
           </button>
           <div className="flex m-2 px-4 items-center cursor-pointer">
             <Image
